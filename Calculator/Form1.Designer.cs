a7a674ce0274c03c8ae0d72c840db23b812f44e2
Schreibi <vitekivanov72@mail.ru>
2014-05-19 04:03:30 +0700

    rename buttons and save project

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,50 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.fold = new System.Windows.Forms.Button();
+            this.deduct = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.share = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // fold
             // 
-            this.button1.Location = new System.Drawing.Point(22, 109);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(48, 38);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.fold.Location = new System.Drawing.Point(22, 109);
+            this.fold.Name = "fold";
+            this.fold.Size = new System.Drawing.Size(48, 38);
+            this.fold.TabIndex = 0;
+            this.fold.Text = "+";
+            this.fold.UseVisualStyleBackColor = true;
+            this.fold.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // deduct
             // 
-            this.button2.Location = new System.Drawing.Point(22, 153);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(48, 38);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.deduct.Location = new System.Drawing.Point(76, 109);
+            this.deduct.Name = "deduct";
+            this.deduct.Size = new System.Drawing.Size(48, 38);
+            this.deduct.TabIndex = 1;
+            this.deduct.Text = "-";
+            this.deduct.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // multiply
             // 
-            this.button3.Location = new System.Drawing.Point(76, 109);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(48, 38);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.multiply.Location = new System.Drawing.Point(22, 153);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(48, 38);
+            this.multiply.TabIndex = 2;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // share
             // 
-            this.button4.Location = new System.Drawing.Point(76, 153);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(48, 38);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.share.Location = new System.Drawing.Point(76, 153);
+            this.share.Name = "share";
+            this.share.Size = new System.Drawing.Size(48, 38);
+            this.share.TabIndex = 3;
+            this.share.Text = "/";
+            this.share.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
@@ -102,10 +103,10 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.share);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.deduct);
+            this.Controls.Add(this.fold);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -115,10 +116,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button fold;
+        private System.Windows.Forms.Button deduct;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button share;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
